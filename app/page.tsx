8c9a1f3115e9c469efd703bedb340465fa8a6374
RATCHAW <bendarsiayoub@gmail.com>
2025-05-10 12:39:54 +0100

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,147 @@
-import Image from "next/image";
+import {
+    Astro,
+    BetterAuth,
+    DrizzleORM,
+    Expressjs,
+    GitHub,
+    LinkedIn,
+    MongoDB,
+    NestJS,
+    Nextjs,
+    PostgreSQL,
+    ReactQuery,
+    Tebex,
+    Vite,
+    XformerlyTwitter,
+} from "@/components/ui/icons";
+import { Separator } from "@/components/ui/separator";
+import { ArrowUpRight } from "lucide-react";
+
+const WHATIUSE = [
+    {
+        name: "Next.js",
+        icon: Nextjs,
+    },
+    {
+        name: "Express.js",
+        icon: Expressjs,
+    },
+    {
+        name: "NestJS",
+        icon: NestJS,
+    },
+    {
+        name: "PostgreSQL",
+        icon: PostgreSQL,
+    },
+    {
+        name: "MongoDB",
+        icon: MongoDB,
+    },
+    {
+        name: "Vite",
+        icon: Vite,
+    },
+    {
+        name: "React Query",
+        icon: ReactQuery,
+    },
+    {
+        name: "Astro",
+        icon: Astro,
+    },
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    return (
+        <main className="min-h-screen p-4 pt-16 leading-relaxed flex flex-col items-center">
+            <div className="max-w-xl gap-y-10 flex flex-col justify-between h-full w-full">
+                <header>
+                    <h1 className="text-xl">Ayoub Bendarsi</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+                    <Separator className="w-full my-2" />
+                    <p className="text-sm">Full Stack Web Developer</p>
+                </header>
+                <section className="flex flex-col gap-y-2">
+                    <div className="flex justify-between items-center">
+                        <h2>About me</h2>
+                        <div className="flex gap-4">
+                            <a href="https://www.linkedin.com/in/ayoub-bendarsi-41740a265/" target="_blank">
+                                <LinkedIn className="size-5" />
+                            </a>
+                            <a href="https://x.com/bendarsiayoub" target="_blank">
+                                <XformerlyTwitter className="size-5" />
+                            </a>
+                            <a href="https://github.com/RATCHAW" target="_blank">
+                                <GitHub className="size-5" />
+                            </a>
+                        </div>
+                    </div>
+                    <Separator className="w-full my-2" />
+                    <p className="text-sm">
+                        Hello, I&apos;m Ayoub Bendarsi, a full-stack web developer passionate about crafting tech-driven
+                        solutions. I specialize in building responsive applications with React, Node.js, and TypeScript.
+                        <br />
+                        <br />
+                        From interactive UIs to robust APIs, I enjoy solving problems with elegant code. Always
+                        learning, always building — I turn ideas into scalable digital products.
+                        <br />
+                        <br />
+                        Feel free to reach out to me ( <a href="mailto:bendarsiayoub@gmail.com">Email</a> /
+                        <a
+                            className="align-middle inline-block mx-1 "
+                            href="https://x.com/bendarsiayoub"
+                            target="_blank"
+                        >
+                            <XformerlyTwitter className="size-3" />
+                        </a>
+                        ) if you have any questions or just want to chat.
+                    </p>
+                </section>
+                <section>
+                    <h2 className="h-10">What I built</h2>
+                    <a href="https://needforscript.com/" target="_blank">
+                        <div className="text-sm border rounded-2xl p-4 transition-shadow group hover:ring-2 hover:ring-primary hover:ring-offset-2 hover:ring-offset-background">
+                            <div className="flex justify-between">
+                                <h3 className="text-sm font-bold tracking-wide">Webstore</h3>
+                                <ArrowUpRight className="group-hover:rotate-45 transtion transition " />
+                            </div>
+                            <Separator className="w-full my-2" />
+                            <p className="my-4 ">
+                                A high-performing webstore, hundreds of transactions monthly. Integrated with Tebex for
+                                secure payments and reliable scaling. Focused on performance, clean UI, and seamless
+                                user experience.
+                            </p>
+                            <Separator className="w-full my-2" />
+                            <div className="flex justify-between">
+                                <h4>Built With</h4>
+                                <div className="flex flex-row-reverse gap-2">
+                                    <Tebex className="size-6" />
+                                    <DrizzleORM className="size-6" />
+                                    <BetterAuth className="size-6" />
+                                    <ReactQuery className="size-6" />
+                                    <Nextjs className="size-6" />
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </section>
+                <section>
+                    <h2>What I use</h2>
+                    <Separator className="w-full my-2" />
+                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 py-4">
+                        {WHATIUSE.map((item) => (
+                            <div
+                                key={item.name}
+                                className="flex text-center flex-col items-center justify-center gap-2 text-sm border rounded-2xl p-4 transition-shadow group hover:ring-2 hover:ring-primary hover:ring-offset-2 hover:ring-offset-background"
+                            >
+                                <item.icon className="size-6" />
+                                <p>{item.name}</p>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </div>
+        </main>
+    );
 }
